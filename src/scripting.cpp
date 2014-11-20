0b67db9238379d4facb162068fef07573af9d222
John Connors <johnc@yagc.ndo.co.uk>
2014-11-20 07:34:10 +0000

    Fixed startup issue.

--- src/scripting.cpp
+++ src/scripting.cpp
@@ -38,6 +38,8 @@ bool ScriptingService::initialise(ScriptingService* self)
 		std::cerr << "Tiny scheme init failed" << std::endl;
 		return nullptr;
 	}
+	scheme_set_output_port_file(sc,stdin);
+	scheme_set_input_port_file(sc,stdin);	
 	for(auto const &f : scheme_files) {
 		load_scheme(sc, f.c_str());
 	}
